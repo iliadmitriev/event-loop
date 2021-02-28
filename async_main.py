f7989d442ed48301d0c3d288b96e8224422fb207
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-28 16:24:36 +0300

    sync vs async

--- async_main.py
+++ async_main.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 import requests
 from pathlib import Path
 from time import time
@@ -25,6 +27,39 @@ def main():
 
     print(f'Download took {time() - t0} sec')
 
+#
+# if __name__ == '__main__':
+#     main()
+
+#######################################################
+
+
+
+def write_image(data):
+    filename = BASE_PATH / 'cache' / 'file-{}.jpg'.format(int(time() * 1000))
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+
+async def fetch_content(url, session):
+    async with session.get(url, allow_redirects=True) as response:
+        data = await response.read()
+        write_image(data)
+
+
+async def main2():
+    url = 'https://loremflickr.com/320/240'
+    tasks = []
+
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            task = asyncio.create_task(fetch_content(url, session))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
 
 if __name__ == '__main__':
-    main()
+    t0 = time()
+    asyncio.run(main2())
+    print(f'{time() - t0}')
